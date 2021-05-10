ba8e9026910a7732b2fd8edcdd110ed9b814aafb
27 <zhengchengzhong@yuewen.com>
2021-05-10 19:38:47 +0800

    feat: jsx-runtime 支持接受数组

--- index.js
+++ index.js
@@ -3,23 +3,39 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports["default"] = _default;
-exports.Fragment = void 0;
+exports.Fragment = exports["default"] = void 0;
 
 /**
  * jsx-runtime 返回的 结果是 {safeHTML:'**'} 的结构
  * @param obj
  * @returns {string|string|*}
  */
-function _default(obj) {
-  if (obj && typeof obj.safeHTML === 'string') {
+function jsx2string(obj) {
+  if (!obj) {
+    return '';
+  } // 普通对象
+
+
+  if (typeof obj.safeHTML === 'string') {
     return obj.safeHTML;
+  } // 如果是数组
+
+
+  if (obj instanceof Array) {
+    var result = '';
+
+    for (var i = 0, len = obj.length; i < len; i++) {
+      result += jsx2string(obj[i]);
+    }
+
+    return result;
   }
 
   return obj;
 }
 
-; // 空标签
+var _default = jsx2string; // 空标签
 
+exports["default"] = _default;
 var Fragment = "Fragment";
 exports.Fragment = Fragment;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,27 @@
  * @param obj
  * @returns {string|string|*}
  */
-export default function (obj) {
-  if (obj && typeof obj.safeHTML === 'string') {
+function jsx2string(obj) {
+  if (!obj) {
+    return '';
+  }
+  // 普通对象
+  if (typeof obj.safeHTML === 'string') {
     return obj.safeHTML;
   }
+  // 如果是数组
+  if (obj instanceof Array) {
+    let result = '';
+    for (let i = 0, len = obj.length; i < len; i++) {
+      result += jsx2string(obj[i]);
+    }
+    return result;
+  }
   return obj;
-};
+}
+
+export default jsx2string;
+
 // 空标签
 const Fragment = "Fragment";
 export {Fragment};
